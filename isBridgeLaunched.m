463f29ab1075e88e60201e93680ba80d611bd685
Antonio Enrique Jimenez Cano <antenr@us>
2019-05-06 11:55:43 +0200

    apdate AP

--- isBridgeLaunched.m
+++
@@ -1,9 +0,0 @@
-function isrunning = isBridgeLaunched()
-    [~,~,~,~,~,nameRaspi] = generatePathandNames();
-    r=raspberrypi(nameRaspi,'pi','raspberry');
-    if ~contains(system(r,'ps -aux'),'bridge_navio')
-       isrunning = false;
-    else
-       isrunning = true;
-    end
-end
--- shutDownRaspi.m
+++
@@ -1,7 +0,0 @@
-function shutDownRaspi()
-    [~,~,~,~,~,nameRaspi] = generatePathandNames();
-    r=raspberrypi(nameRaspi,'pi','raspberry');  
-    system(r,'sudo shutdown -h now +1');
-    % Shut down
-    %r.execute('sudo shutdown -r now'); % Reboot 
- end
